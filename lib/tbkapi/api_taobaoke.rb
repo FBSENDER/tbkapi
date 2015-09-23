b3aaae2e36b440dd4eff58f650919835901061b7
刘梦晨 <mengchen.liu@corp.elong.com>
2015-09-23 19:03:22 +0800

    tbk item detail get api

--- lib/tbkapi/api_taobaoke.rb
+++ lib/tbkapi/api_taobaoke.rb
@@ -14,5 +14,15 @@ module Tbkapi
       HTTParty.get(TBURI, query: params.merge({sign: my_sign}))
     end
 
+    def taobao_tbk_items_detail_get(ids, api_key, secret)
+      action_params = {
+        fields: "num_iid,seller_id,nick,title,volume,pic_url,item_url,shop_url,click_url,price,discount_price",
+        num_iids: ids.join(",")
+      }
+      params = system_params("taobao.tbk.items.detail.get",api_key).merge(action_params)
+      my_sign = get_sign(params, secret).upcase
+      HTTParty.get(TBURI, query: params.merge({sign: my_sign}))
+    end
+
   end
 end
