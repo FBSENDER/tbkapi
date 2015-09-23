fc4d5f651803f28ecd5f536976c39882b2651f21
刘梦晨 <mengchen.liu@corp.elong.com>
2015-09-23 14:47:45 +0800

    add tbk items get

--- lib/tbkapi.rb
+++ lib/tbkapi.rb
@@ -1,5 +1,29 @@
+require "httparty"
+require "digest/md5"
+require "tbkapi/api_taobaoke"
 require "tbkapi/version"
 
 module Tbkapi
-  # Your code goes here...
+
+  TBURI = "http://gw.api.taobao.com/router/rest/"
+  
+  def system_params(method, api_key)
+    {
+      method: method,
+      api_key: api_key,
+      timestamp: Time.now.strftime("%F %T"),
+      format: "json",
+      v: "2.0",
+      sign_method: "md5"
+    }
+  end
+
+  def action_params
+    {}
+  end
+
+  def get_sign(hash, secret)
+    Digest::MD5.hexdigest(hash.sort.flatten.push(secret).unshift(secret).join(""))
+  end
+
 end
---
+++ lib/tbkapi/api_taobaoke.rb
@@ -0,0 +1,18 @@
+module Tbkapi
+  class Taobaoke
+
+    include Tbkapi
+
+    def taobao_tbk_items_get(keyword, api_key, secret, page_size = 20)
+      action_params = {
+        fields: "num_iid,seller_id,nick,title,volume,pic_url,item_url,shop_url,click_url",
+        keyword: keyword,
+        page_size: page_size
+      }
+      params = sys_params("taobao.tbk.items.get",api_key).merge(action_params)
+      my_sign = get_sign(params, secret).upcase
+      HTTParty.get(TBURI, query: params.merge({sign: my_sign}))
+    end
+
+  end
+end
